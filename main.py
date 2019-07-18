1959f171d8d61c9a61f2f9639797f1481855aaa0
Liora <lioralehrer@gmail.com>
2019-07-18 16:02:03 +0300

    browes brings the data :)

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import os
-from bottle import (get, post, redirect, request, route, run, static_file,
-                    template)
+from bottle import (get, post, redirect, request,
+                    route, run, static_file, template)
 import utils
+import json
+
 
 # Static Routes
+
 
 @get("/js/<filepath:re:.*\.js>")
 def js(filepath):
     return static_file(filepath, root="./js")
 
+
 @get("/css/<filepath:re:.*\.css>")
 def css(filepath):
     return static_file(filepath, root="./css")
+
 
 @get("/images/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
 def img(filepath):
     return static_file(filepath, root="./images")
 
+
 @route('/')
 def index():
     sectionTemplate = "./templates/home.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+
 
 @route('/browse')
-def index():
+def browse():
     sectionTemplate = "./templates/browse.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})
-@route('/search')
-def index():
-    sectionTemplate = "./templates/search.tpl"
-    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData = {})   
-if __name__ == "__main__":
-    run(host='localhost', port=os.environ.get('PORT', 5000),reloader=True,debug=True)
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData=utils.getShowsData())
 
+
+@route('/search')
+def search():
+    sectionTemplate = "./templates/search.tpl"
+    return template("./pages/index.html", version=utils.getVersion(), sectionTemplate=sectionTemplate, sectionData={})
+
+
+if __name__ == "__main__":
+    run(host='localhost', port=os.environ.get(
+        'PORT', 5000), reloader=True, debug=True)
